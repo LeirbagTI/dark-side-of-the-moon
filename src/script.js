250657bb9b606b44c366efce423b78788c07ad83
Gabriel Campos <gcnv1415@gmail.com>
2023-09-02 16:27:50 -0300

    Site
    Fiz por diversão. Sou fã da banda (pasmem)

---
+++ src/script.js
@@ -0,0 +1,9 @@
+let moon = document.getElementById("cover")
+let texto = document.getElementById("texto")
+
+window.addEventListener('scroll', function(){
+    let value = window.scrollY
+    
+    moon.style.top = value * 0.6 + 'px';
+    texto.style.bottom = value * 0.6 + 'px';
+});
