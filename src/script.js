b1820d21a64d297e6cae6223b15bb4be9b9d34c4
Gabriel Campos <gcnv1415@gmail.com>
2023-09-06 14:31:19 -0300

    Espaçamento entre spotify e musicas

--- src/script.js
+++ src/script.js
@@ -5,5 +5,5 @@ window.addEventListener('scroll', function(){
     let value = window.scrollY
     
     moon.style.top = value * 0.6 + 'px';
-    texto.style.bottom = value * 0.6 + 'px';
+    texto.style.bottom = value * 0.1 + 'px';
 });
